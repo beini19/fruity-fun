85fb2b4a55aea826ed93a7b185846799a032ee1d
Ding Bei Ni Fang <dbnfang@uwaterloo.ca>
2017-10-25 21:57:12 -0400

    Add touchscreen feature, grids are highlighted when touched

--- prototype/prototype.ino
+++ prototype/prototype.ino
@@ -64,11 +64,11 @@ void setup() {
   tft.begin(identifier);
     
   //background
-    tft.fillScreen(WHITE);
+    tft.fillScreen(BLACK);
     delay(2000);
 
-  // set up grid array of 8 x 6 objects
-  int grid[6][6];
+  // set up grid array of 5 x 6 objects
+  int grid[5][6];
 
 //  tft.fillCircle(100, 100, 40, GREEN);
 //  delay(1000);
@@ -92,15 +92,26 @@ PURPLE = 5
 */
 
 //Initialization variables
-int cSize = 10;
-int elementDistance = 40;
-int x1Border = elementDistance/2;
+#define cSize 10
+#define elementDistance 40
+#define x1Border 20;
+
+#define TS_MINX 120
+#define TS_MAXX 900
+
+#define TS_MINY 70
+#define TS_MAXY 920
+
+#define MINPRESSURE 10
+#define MAXPRESSURE 1000
+  
+
 //Draw grid
   for (int i = 0; i <= 5; i++) {
-    for (int j = 0; j <= 6; j++) {
-      tft.drawFastVLine(i*elementDistance + 20, 20, 240, BLACK);
-      tft.drawFastHLine(20, j*elementDistance + 20, 200, BLACK);
-    }
+      tft.drawFastVLine(i*elementDistance + 20, 20, 240, WHITE);
+  }
+  for (int j = 0; j <= 6; j++) {
+      tft.drawFastHLine(20, j*elementDistance + 20, 200, WHITE);
   }
       
   for (int i = 0; i <= 4; i++) {
@@ -123,11 +134,65 @@ int x1Border = elementDistance/2;
     }
 }
 
-      
+ 
+}//end of setup
 
-
-}
 
 void loop() {
- // TSPoint p = ts.getPoint();
+
+  digitalWrite(13, HIGH);
+  TSPoint p = ts.getPoint();
+  digitalWrite(13, LOW);
+
+  pinMode(XM, OUTPUT);
+  pinMode(YP, OUTPUT);
+
+  bool isTouched[5][6] = {false}; //Boolean values are true when area of screen is being pressed
+  
+  //Redraw elements on screen everytime user touches something? Probably a bad idea, find some way to make 'grid' a global variable
+ 
+  if (p.z > MINPRESSURE && p.z < MAXPRESSURE) {
+    
+    // scale from 0->1023 to tft.width
+    p.x = map(p.x, TS_MINX, TS_MAXX, tft.width(), 0);
+    p.y = (tft.height()-map(p.y, TS_MINY, TS_MAXY, tft.height(), 0));
+//    bool isTouching(void);
+
+int selectElement = 0;
+
+//Check each grid to see if it's been pressed
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 6; j++) {
+      if (p.x > (20 + elementDistance*i) && p.x < (20 + elementDistance*(i+1))
+       && p.y > (40 + elementDistance*j) && p.y < (40 + elementDistance*(j+1))) {
+          isTouched[i][j] = true;
+          selectElement++;
+          if (selectElement <= 2) {
+           tft.drawRect(tft.width() - (20 + elementDistance*(i+1)),tft.height() - (20 + elementDistance*(j+2)), 
+           elementDistance + 1, elementDistance + 1, RED);
+          }
+          else if (selectElement > 2) {
+              for (int i = 0; i <= 5; i++) {
+                 tft.drawFastVLine(i*elementDistance + 20, 20, 240, WHITE);
+              }
+              for (int j = 0; j <= 6; j++) {
+                  tft.drawFastHLine(20, j*elementDistance + 20, 200, WHITE);
+               }      
+             }
+           }
+        }
+      
+    }
+  }
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 6; j++) {
+      if (isTouched[i][j]) {
+        Serial.print("Grid "); Serial.print(i); Serial.print(" "); Serial.println(j);  //Print grid
+        
+        //Highlight the grid that's been pressed
+        
+    }
+  }
+       
+  }
 }
